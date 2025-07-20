40d47a3874271f96f6accee9ec0bb7894fd15926
Maxwell Vale <max.s.vale01@gmail.com>
2025-07-19 20:53:26 -0700

    More comments

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 // DISREGARD THIS FILE
 
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import Home from './pages/Home.jsx';
+// import Header from './components/Header.jsx';
+// import Footer from './components/Footer.jsx';
+// import Home from './pages/Home.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <Home />
-      </main>
-      <Footer />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <main>
+//         <Home />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // DISREGARD THIS FILE
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles/App.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import './styles/App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
