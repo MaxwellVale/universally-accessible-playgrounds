de5c2af45af0c8903bdc17c261dfc32d6972977d
Maxwell Vale <max.s.vale01@gmail.com>
2025-07-20 18:26:22 -0700

    Renamed page Playgrounds. Sidebar list is now standard for playgrounds and playground layouts

---
+++ src/pages/Playgrounds.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import PlaygroundsMap from '../components/PlaygroundsMap.jsx';
+import { playgrounds } from '../data/playgroundData.js';
+
+export default function Playgrounds() {
+  return (
+    <>
+      <h2 className='info-heading'>Universally Accessible Playgrounds</h2>
+      <p className='description'>Find playgrounds that are accessible for everyone in your community.</p>
+      <div className='playground-map'>
+        <PlaygroundsMap playgrounds={ playgrounds } />
+      </div>
+    </>
+  );
+}
